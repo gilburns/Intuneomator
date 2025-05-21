8c96cf1ecfc67304409bfb98a66c085e28dee4e1
Gil Burns <gil@gilburns.com>
2025-05-20 20:52:15 -0500

    Update Package_Build.sh

--- Package/Package_Build.sh
+++ Package/Package_Build.sh
@@ -6,6 +6,68 @@
 #  Created by Gil Burns on 5/20/25.
 #  
 
-identifier="com.gilburns.Intuneomator"
-installLocation="/Applications"
+set -e  # Exit on error
 
+# === Variables ===
+APP_NAME="Intuneomator"
+VERSION="1.0"
+IDENTIFIER="com.gilburns.Intuneomator"
+INSTALL_LOCATION="/Applications"
+TMP_DIR="./Root"
+SCRIPTS_DIR="./Scripts"
+PKG_NAME="${APP_NAME}.pkg"
+SIGNED_PKG_NAME="${APP_NAME}-${VERSION}.pkg"
+COMPONENT_PLIST="component.plist"
+DIST_XML="Distribution.xml"
+SIGN_ID="Developer ID Installer: Gil Burns (G4MQ57TVLE)" # <-- Change this
+
+# Fix this path for the final release. This is for testing only
+BUILD_PATH="/Users/gilburns/Library/Developer/Xcode/DerivedData/Intuneomator-goaiftwymseounhbjldcvcytweic/Build/Products/Debug"
+
+# Insure that the scripts are executable
+chmod 755 "${SCRIPTS_DIR}/preinstall"
+chmod 755 "${SCRIPTS_DIR}/postinstall"
+
+# === Cleanup ===
+rm -rf "$PKG_NAME" "$SIGNED_PKG_NAME" "$COMPONENT_PLIST" "$DIST_XML"
+
+# === Create staging root ===
+mkdir -p "$TMP_DIR/Applications"
+mkdir -p "$TMP_DIR/Library/Application Support/Intuneomator"
+
+cp -R "${BUILD_PATH}/${APP_NAME}.app" "$TMP_DIR${INSTALL_LOCATION}"
+cp "${BUILD_PATH}/IntuneomatorService" "$TMP_DIR/Library/Application Support/Intuneomator/"
+
+chmod 755 "$TMP_DIR/Library/Application Support/Intuneomator/IntuneomatorService"
+
+# === Remove any .DS_Store files ===
+echo "Cleaning up .DS_Store files..."
+find "$TMP_DIR" -name ".DS_Store" -type f -delete
+find "$SCRIPTS_DIR" -name ".DS_Store" -type f -delete
+
+# === Analyze app to generate component plist ===
+/usr/bin/pkgbuild --analyze --root "$TMP_DIR" "$COMPONENT_PLIST"
+
+# === Modify component plist to set BundleIsRelocatable to false ===
+/usr/bin/plutil -replace "BundleIsRelocatable" -bool false "$COMPONENT_PLIST"
+
+# === Build component package ===
+/usr/bin/pkgbuild \
+  --root "$TMP_DIR" \
+  --identifier "$IDENTIFIER" \
+  --version "$VERSION" \
+  --scripts "$SCRIPTS_DIR" \
+  --component-plist "$COMPONENT_PLIST" \
+  "$PKG_NAME"
+
+# === Generate Distribution XML ===
+/usr/bin/productbuild --synthesize --package "$PKG_NAME" "$DIST_XML"
+
+# === Build final signed package ===
+/usr/bin/productbuild \
+  --distribution "$DIST_XML" \
+  --package-path "." \
+  --sign "$SIGN_ID" \
+  "$SIGNED_PKG_NAME"
+
+echo "Created signed package: $SIGNED_PKG_NAME"
