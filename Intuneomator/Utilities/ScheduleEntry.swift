a7103c15cdd59765a92f40f820e627fccde984b2
Gil Burns <gil@gilburns.com>
2025-05-22 20:40:10 -0500

    Update ScheduleEntry.swift

--- Intuneomator/Utilities/ScheduleEntry.swift
+++ Intuneomator/Utilities/ScheduleEntry.swift
@@ -8,24 +8,23 @@
 import Foundation
 
 class ScheduleEntry: NSObject {
-    var weekday: Int?  // 1 = Sunday ... 7 = Saturday
+    var weekday: Weekday?
     var hour: Int
     var minute: Int
 
-    init(weekday: Int?, hour: Int, minute: Int) {
+    init(weekday: Weekday?, hour: Int, minute: Int) {
         self.weekday = weekday
         self.hour = hour
         self.minute = minute
     }
 
     func displayString() -> String {
-        let weekdayNames = [nil, "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"]
-        let weekdayPart = weekday.flatMap { weekdayNames[$0] } ?? "Daily"
+        let weekdayPart = weekday?.name ?? "Daily"
         let timePart = String(format: "%02d:%02d", hour, minute)
         return "\(weekdayPart) @ \(timePart)"
     }
 
     func toScheduledTime() -> ScheduledTime {
-        return ScheduledTime(weekday: weekday.map { NSNumber(value: $0) }, hour: hour, minute: minute)
+        return ScheduledTime(weekday: weekday.map { NSNumber(value: $0.rawValue) }, hour: hour, minute: minute)
     }
 }
