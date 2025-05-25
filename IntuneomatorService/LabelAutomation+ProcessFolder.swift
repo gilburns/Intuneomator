4632636568a75ac5c4850ddf65f6a9c29a9488a1
Gil Burns <gil@gilburns.com>
2025-05-25 13:38:56 -0500

    Update LabelAutomation+ProcessFolder.swift

--- IntuneomatorService/LabelAutomation+ProcessFolder.swift
+++ IntuneomatorService/LabelAutomation+ProcessFolder.swift
@@ -269,6 +269,16 @@ extension LabelAutomation {
             let entraAuthenticator = EntraAuthenticator()
             let authToken = try await entraAuthenticator.getEntraIDToken()
             
+            guard let localFilePath = processedAppResults?.appLocalURL else {
+                Logger.log("No file to upload to Intune", logType: logType)
+                return
+            }
+            
+            guard FileManager.default.fileExists(atPath: localFilePath) else {
+                Logger.log("File does not exist", logType: logType)
+                return
+            }
+            
             // Call the upload function
             try await EntraGraphRequests.uploadAppToIntune(authToken: authToken, app: processedAppResults!)
             
@@ -400,123 +410,10 @@ extension LabelAutomation {
         
         // MARK: - Send Teams Notification
         
-        // Get the Teams notification state from Config
-        let sendTeamNotification = ConfigManager.readPlistValue(key: "TeamsNotificationsEnabled") ?? false
-
-        // If we should send a notification
-        if sendTeamNotification {
-            // get the webhook URL
-            let url = ConfigManager.readPlistValue(key: "TeamsWebhookURL") ?? ""
-            
-            if url.isEmpty {
-                Logger.log("No Teams Webhook URL set in Config. Not sending notification.", logType: logType)
-            } else {
-                
-                // Set the Icon for the notification
-                let iconImageURL = "https://icons.intuneomator.org/\(processedAppResults?.appLabelName ?? "genericapp").png"
-                
-                // Get file size for Teams Notification
-                var fileSizeDisplay: String = ""
-                do {
-                    let fileAttributes = try FileManager.default.attributesOfItem(atPath: processedAppResults?.appLocalURL ?? "")
-                    let fileSizeBytes = fileAttributes[.size] as? Int64 ?? 0
-                    let fileSizeMB = Double(fileSizeBytes) / 1_048_576
-
-                    if fileSizeMB >= 1000 {
-                        let fileSizeGB = fileSizeMB / 1024
-                        fileSizeDisplay = String(format: "%.2f GB", fileSizeGB)
-                    } else {
-                        fileSizeDisplay = String(format: "%.2f MB", fileSizeMB)
-                    }
-
-                    Logger.log("File size: \(fileSizeDisplay)", logType: logType)
-                } catch {
-                    Logger.log("Unable to get file size: \(error.localizedDescription)", logType: logType)
-                }
-
-                // Get time stamp for notification:
-                let formatter = DateFormatter()
-                formatter.dateStyle = .medium
-                formatter.timeStyle = .short
-                let dateString = formatter.string(from: Date())
-
-                let friendlyDate = "🕰️ \(dateString)"
-
-                // Set the deployment type for the notification
-                var deploymentType: String = ""
-                switch processedAppResults?.appDeploymentType ?? 0 {
-                case 0:
-                    deploymentType = "💾 DMG"
-                case 1:
-                    deploymentType = "📦 PKG"
-                case 2:
-                    deploymentType = "🏢 LOB"
-                default:
-                    deploymentType = "🔄 Unknown"
-                }
-                
-                // Set the deployment arch for the notification
-                var deploymentArch: String = ""
-                switch processedAppResults?.appDeploymentArch ?? 0 {
-                    case 0:
-                    deploymentArch = "🌍 Arm64"
-                case 1:
-                    deploymentArch = "🌍 x86_64"
-                case 2:
-                    deploymentArch = "🌍 Universal"
-                default:
-                    deploymentArch = "🔄 Unknown"
-                }
-                
-                let assignedGroups = processedAppResults?.appAssignments ?? []
-                
-                
-                // We were sucessful with the upload
-                if uploadSucceeded {
-                    do {
-                        
-                        // Initilaize the Notifier
-                        let teamsNotifier = TeamsNotifier(webhookURL: url)
-                        
-                        // Send the notification (Currently missing some attributes)
-                        teamsNotifier.sendNotification(
-                            title: processedAppResults?.appDisplayName ?? "",
-                            version: processedAppResults?.appVersionActual ?? "",
-                            size: fileSizeDisplay,
-                            time: friendlyDate,
-                            imageURL: iconImageURL,
-                            deploymentType: deploymentType,
-                            architecture: deploymentArch,
-                            releaseNotesURL: nil,
-                            assignedGroups: assignedGroups,
-                            isSuccess: true
-                        )
-                    }
-                } else {
-                    do {
-                        
-                        // Initilaize the Notifier
-                        let teamsNotifier = TeamsNotifier(webhookURL: url)
-                        
-                        // Send the notification (Currently missing some attributes)
-                        teamsNotifier.sendNotification(
-                            title: processedAppResults?.appDisplayName ?? "",
-                            version: processedAppResults?.appVersionActual ?? "",
-                            size: fileSizeDisplay,
-                            time: friendlyDate,
-                            imageURL: iconImageURL,
-                            deploymentType: deploymentType,
-                            architecture: deploymentArch,
-                            releaseNotesURL: nil,
-                            assignedGroups: assignedGroups,
-                            isSuccess: false,
-                            errorMessage: "Failed to upload to Intune. The automation will try again the next time it runs."
-                        )
-                    }
-                }
-            }
-        } else {
-            Logger.log("❌ Teams notifications are not enabled.", logType: logType)
+        let sendNotification = TeamsNotifier.processNotification(for: processedAppResults, success: uploadSucceeded)
+        
+        if sendNotification {
+            Logger.log("Message sent to Teams channel")
         }
         
         
