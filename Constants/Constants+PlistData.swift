fdcd5cf8f7e2d895cb1623f355d061fda651bd20
Gil Burns <gil@gilburns.com>
2025-05-30 11:36:16 -0500

    Create Constants+PlistData.swift

---
+++ Constants/Constants+PlistData.swift
@@ -0,0 +1,19 @@
+//
+//  Constants+PlistData.swift
+//  Intuneomator
+//
+//  Created by Gil Burns on 3/14/25.
+//
+
+import Foundation
+
+// Define a struct to store extracted plist data
+struct PlistData {
+    let appNewVersion: String?
+    let downloadURL: String
+    let expectedTeamID: String
+    let name: String
+    let type: String
+}
+
+
