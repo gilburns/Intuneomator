a42ca3a1b1a8df6302ac810880a2f1c49d79e0dc
Gil Burns <gil@gilburns.com>
2025-06-02 16:43:01 -0500

    Create TeamsNotifier+NotifyUpdateAvailable.swift

---
+++ IntuneomatorService/TeamsNotifier+NotifyUpdateAvailable.swift
@@ -0,0 +1,136 @@
+//
+//  TeamsNotifier+NotifyUpdate.swift
+//  Intuneomator
+//
+//  Created by Gil Burns on 2/19/25.
+//
+
+import Foundation
+
+extension TeamsNotifier {
+    
+    func sendUpdateAvailableNotification(
+        initialVersion: String,
+        updatedVersion: String,
+        errorMessage: String? = nil
+    ) {
+        let title = "Intuneomator Service"
+        let intuneomatorIconUrl: String = "https://icons.intuneomator.org/intuneomator.png"
+        
+        let versionFacts: [[String: String]] = [
+            ["title": "Initial Version:", "value": initialVersion],
+            ["title": "Updated Version:", "value": updatedVersion]
+        ]
+                
+        var bodyContent: [[String: Any]] = [
+            [
+                "type": "ColumnSet",
+                "columns": [
+                    [
+                        "type": "Column",
+                        "items": [
+                            ["type": "Image", "url": intuneomatorIconUrl, "size": "Medium"]
+                        ],
+                        "width": "auto"
+                    ],
+                    [
+                        "type": "Column",
+                        "items": [
+                            ["type": "TextBlock", "text": " ", "weight": "Bolder", "size": "Medium"]
+                        ],
+                        "width": "stretch"
+                    ],
+                    [
+                        "type": "Column",
+                        "items": [
+                            [
+                                "type": "FactSet",
+                                "facts": [
+                                    [
+                                        "title": " ",
+                                        "value": "🟢 **UPDATE AVAILABLE**",
+                                        "color": "Good",
+                                    ]
+                                ]
+                            ]
+                        ],
+                        "width": "auto",
+                        "style": "good",
+                    ]
+                ]
+            ],
+            [
+                "type": "TextBlock",
+                "text": "**\(title)**",
+                "weight": "Bolder",
+                "spacing": "None",
+                "size": "Large"
+            ],
+            [
+                "type": "TextBlock",
+                "text": "Intuneomator service has an available update.",
+                "weight": "Lighter",
+                "spacing": "Small",
+                "size": "Small"
+            ],
+            [
+                "type": "TextBlock",
+                "text": "---",
+                "weight": "Lighter",
+                "spacing": "Medium",
+                "separator": true
+            ],
+            [
+                "type": "TextBlock",
+                "text": "**Version Information:**",
+                "weight": "Bolder",
+                "size": "Medium",
+                "spacing": "Medium"
+            ],
+            [
+                "type": "FactSet",
+                "facts": versionFacts
+            ],
+            [
+                "type": "TextBlock",
+                "text": "---",
+                "weight": "Lighter",
+                "spacing": "Medium",
+                "separator": true
+            ]
+        ]
+        
+        // Add possible error info
+        if let errorMessage = errorMessage, !errorMessage.trimmingCharacters(in: .whitespaces).isEmpty {
+            bodyContent.append([
+                "type": "TextBlock",
+                "text": "**Error Details**",
+                "weight": "Bolder",
+                "size": "Medium",
+                "color": "Attention",
+                "spacing": "Medium"
+            ])
+            
+            bodyContent.append([
+                "type": "Container",
+                "items": [
+                    [
+                        "type": "TextBlock",
+                        "text": errorMessage,
+                        "wrap": true,
+                        "color": "Attention",
+                        "spacing": "Small"
+                    ]
+                ],
+                "style": "attention",
+                "bleed": true
+            ])
+        }
+
+        // Send Teams Notification
+        self.sendTeamsNotification(bodyContent: bodyContent)
+
+    }
+
+}
+
