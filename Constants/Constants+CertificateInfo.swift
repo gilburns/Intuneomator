2b4fa1dfcd4f198a94b4dc3ed623b5fe089a038d
Gil Burns <gil@gilburns.com>
2025-06-06 10:19:10 -0500

    Update Constants+CertificateInfo.swift

--- Constants/Constants+CertificateInfo.swift
+++ Constants/Constants+CertificateInfo.swift
@@ -7,14 +7,33 @@
 
 import Foundation
 
+/// Certificate information structure for Microsoft Entra authentication
+/// Used for certificate-based authentication with Microsoft Graph API
 struct CertificateInfo {
+    /// Subject name from the certificate (CN=, OU=, etc.)
     let subjectName: String
+    
+    /// Unique serial number assigned by the certificate authority
     let serialNumber: String
+    
+    /// Certificate authority that issued this certificate
     let issuerName: String
+    
+    /// SHA-1 thumbprint/fingerprint of the certificate
     let thumbprint: String
+    
+    /// Certificate expiration date (nil if not available)
     let expirationDate: Date?
+    
+    /// Key usage flags indicating certificate purpose
     let keyUsage: Int?
+    
+    /// DNS subject alternative names from the certificate
     let dnsNames: [String]?
+    
+    /// Public key algorithm (RSA, ECDSA, etc.)
     let algorithm: String?
+    
+    /// Public key size in bits
     let keySize: Int?
 }
